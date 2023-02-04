29e3bd7ed2f6b8deacea3736f861a9bc8412ed88
Ved <vedbulsara7@gmail.com>
2023-02-04 22:18:42 +0530

    commit1(4/2/23)

---
+++ int_io.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1;
+    cout << "Enter a number : " <<endl;
+    cin >> num1;
+    cout << "The number is : " << num1;
+}
---
+++ print_var_char.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int num1 = 44;
+    double num2 = 44.63;
+    char ch = 'A';
+
+    cout << num1 << endl;
+    cout << num2 << endl;
+    cout << "Character : " << ch;
+}
